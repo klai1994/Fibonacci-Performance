b80d4a65ccd17422df6cc602a44034843c86a7b3
Kevin <klai1994@gmail.com>
2021-06-13 10:08:38 -0400

    added documentation

--- src/com/kevin/FibonnaciRecorder.java
+++ src/com/kevin/FibonnaciRecorder.java
@@ -1,3 +1,4 @@
+package com.kevin;
 import java.io.*;
 import java.util.ArrayList;
 
@@ -9,46 +10,62 @@ import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.chart.ChartUtilities;
 
-public class Main {
+/**
+ * 
+ * @author Kevin Lai
+ * 
+ */
+
+public class FibonnaciRecorder {
 
 	static final int terms = 20;
 	
 	// First 10 terms of Fibonnaci sequence:
-	// 0 1 1 2 3 5 8 13 21 34
+
+	/**
+	 * The main method stores data from the Fibonacci methods as well as the times needed to execute them.
+	 * It also will store the actual Fibonacci sequence to pass to the
+	 * {@link #recordData(ArrayList, ArrayList, ArrayList)} method.
+	 */
 	public static void main(String[] args) {
 
 		ArrayList<Long> iterativeTimes = new ArrayList<Long>();
 		ArrayList<Long> recursiveTimes = new ArrayList<Long>();
 		ArrayList<Integer> results = new ArrayList<Integer>();
 
-		long startTime = 0, endTime = 0, totalTime = 0;
+		long startTime = 0, endTime = 0;
 		
 		for (int i = 0; i <= terms; i++) {	
 			
 			startTime = System.nanoTime();
 			fibonacciIterative(i);
-			endTime = System.nanoTime();
-			totalTime = endTime - startTime;		
-			iterativeTimes.add(totalTime);
-			System.out.println("Iterative result for " + i + " terms: "  + totalTime);
+			endTime = System.nanoTime();	
+			iterativeTimes.add(endTime - startTime);
+			// System.out.println("Iterative result for " + i + " terms: "  + endTime - startTime);
 	
 			startTime = System.nanoTime();
 			fibonacciRecursive(i);
 			endTime = System.nanoTime();
-			totalTime = endTime - startTime;
-			recursiveTimes.add(totalTime);
-			System.out.println("Recursive result for " + i + " terms: "  + totalTime);
+			recursiveTimes.add(endTime - startTime);
+			// System.out.println("Recursive result for " + i + " terms: "  + endTime - startTime);
 			
 			results.add(fibonacciIterative(i));
 			
 		}
 		
-		createChart(iterativeTimes, recursiveTimes, results);
+		createChart(iterativeTimes, recursiveTimes);
 		recordData(iterativeTimes, recursiveTimes, results);
 		
 	}
-
-	static void createChart(ArrayList<Long> iterativeTimes, ArrayList<Long> recursiveTimes, ArrayList<Integer> results) {
+	
+	/**
+	 * Using this method exports a line chart to a .png file with results from the sequences. 
+	 * The x axis represents the number of terms used, and the y axis represents time 
+	 * taken to execute the method in nanoseconds.
+	 * @param iterativeTimes The time data for the iterative method.
+	 * @param recursiveTimes The time data for the recursive method.
+	 */
+	public static void createChart(ArrayList<Long> iterativeTimes, ArrayList<Long> recursiveTimes) {
 
 		XYSeriesCollection seriesCollection =  new XYSeriesCollection();
 		XYSeries iterativeSeries = new XYSeries("Iterative");
@@ -80,7 +97,13 @@ public class Main {
 		}
 	}
 	
-	static void recordData(ArrayList<Long> iterativeTimes, ArrayList<Long> recursiveTimes, ArrayList<Integer> results) {
+	/**
+	 * This method exports a text file with results from the sequence, with the Fibonacci sequence itself as refrence.
+	 * @param iterativeTimes The time data for the iterative method.
+	 * @param recursiveTimes The time data for the recursive method.
+	 * @param results The Fibonacci sequence, recorded by using the iterative method.
+	 */
+	public static void recordData(ArrayList<Long> iterativeTimes, ArrayList<Long> recursiveTimes, ArrayList<Integer> results) {
 		
 		try {
 			Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("data.txt"), "utf-8"));
@@ -100,7 +123,12 @@ public class Main {
 		
 	}
 	
-	static int fibonacciIterative(int n) {
+	/**
+	 * This is the iterative implementation of the Fibonacci sequence.
+	 * @param n The number of terms to be used.
+	 * @return Returns the nth number in the sequence.
+	 */
+	public static int fibonacciIterative(int n) {
 
 		if (n == 0 || n == 1)
 			return n;
@@ -115,7 +143,12 @@ public class Main {
 		return result;
 	}
 
-	static int fibonacciRecursive(int n) {
+	/**
+	 * This is the recursive implementation of the Fibonacci sequence.
+	 * @param n The number of terms to be used.
+	 * @return Returns the nth number in the sequence.
+	 */
+	public static int fibonacciRecursive(int n) {
 
 		if (n == 0 || n == 1)
 			return n;
