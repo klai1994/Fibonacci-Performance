a162db0b5e21aebf1e01426ac21dc8161d8d9e41
Kevin <klai1994@gmail.com>
2021-06-13 09:14:00 -0400

    added chart function

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
 import java.util.ArrayList;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.chart.ChartUtilities;
 
 public class Main {
 
-	static final int terms = 30;
+	static final int terms = 20;
 	
 	// First 10 terms of Fibonnaci sequence:
 	// 0 1 1 2 3 5 8 13 21 34
@@ -38,10 +42,44 @@ public class Main {
 			results.add(fibonacciIterative(i));
 			
 		}
+		
+		createChart(iterativeTimes, recursiveTimes, results);
 		recordData(iterativeTimes, recursiveTimes, results);
 		
 	}
 
+	static void createChart(ArrayList<Long> iterativeTimes, ArrayList<Long> recursiveTimes, ArrayList<Integer> results) {
+
+		XYSeriesCollection seriesCollection =  new XYSeriesCollection();
+		XYSeries iterativeSeries = new XYSeries("Iterative");
+		XYSeries recursiveSeries = new XYSeries("Recursive");
+		for (int i = 0; i <= terms; i++) {
+			iterativeSeries.add(i, iterativeTimes.get(i));
+			recursiveSeries.add(i, recursiveTimes.get(i));
+		}
+		seriesCollection.addSeries(iterativeSeries);
+		seriesCollection.addSeries(recursiveSeries);
+		XYDataset dataset = seriesCollection;
+		
+		JFreeChart chart = ChartFactory.createXYLineChart(
+				"Performance of Fibonacci Methods", 
+				"Terms", 
+				"Times (nanoseconds)",
+				dataset,
+				PlotOrientation.VERTICAL,
+				true,
+				true, 
+				false);
+		
+		int width = 1024;
+		int height = 768;	
+		try {
+			ChartUtilities.saveChartAsPNG(new File("fib_chart.png"), chart, width, height);		
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+		}
+	}
+	
 	static void recordData(ArrayList<Long> iterativeTimes, ArrayList<Long> recursiveTimes, ArrayList<Integer> results) {
 		
 		try {
@@ -49,9 +87,9 @@ public class Main {
 			writer.write("Summary (total terms: " + terms + ")\n-----");
 			for (int i = 0; i <= terms; i++) {
 				writer.write("\nNumber of terms: " + i + 
+						"\nResult: " + results.get(i) +
 						"\nIterative time: " + iterativeTimes.get(i) + 
 						"\nRecursive time: " + recursiveTimes.get(i) + 
-						"\nResult: " + results.get(i) +
 						"\n");
 			}
 			writer.close();
